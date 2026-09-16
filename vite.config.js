f2ca6e689f7e6a34aceacbb19e1a6ba4f816c727
Butiza <butinyana2017@gmail.com>
2026-09-16 12:03:43 +0200

    Configure gh-pages deployment and Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Jammming/',
   plugins: [react()],
+  base: '/Jammming/',
   server: {
     host: true,
   },
